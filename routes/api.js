0e6bfe66843a7915ca01bb7f08fedf2b60984860
Tali <taliitacamargo@icloud.com>
2022-01-29 14:23:18 -0500

    api routes

--- routes/api.js
+++ routes/api.js
@@ -1,3 +1,31 @@
 const router = require("express").Router();
 const Workout = require("../models/index");
 
+
+
+router.get("/api/workouts", (req, res) => {
+    Workout.aggregate9([
+        {
+            $addFields: {
+                totalDuration: { $sum: "$exercises.duration"},
+            },
+        },
+    ])
+    .then((workout) => {
+        res.json(workout);
+    })
+    .catch((err) => {
+        res.json(err)
+    });
+});
+
+router.post("/api/workouts", (req, res) => {
+    Workout.create(req.body)
+    .then((workout) => {
+        res.json(workout);
+    })
+    .catch((err) => {
+        res.json(err)
+    });
+});
+
