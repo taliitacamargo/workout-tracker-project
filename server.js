ad86fa146085907c35da1cbfb394e52634285181
Tali <taliitacamargo@icloud.com>
2022-01-29 12:49:10 -0500

    adding server/models/routes files

---
+++ server.js
@@ -0,0 +1,32 @@
+const dotenv = require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const apiRoutes = require("./routes/api");
+const viewRoutes = require("./routes/view")
+
+
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+app.use(express.static("public"));
+
+app.use(apiRoutes);
+app.use(viewRoutes)
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+  });
+
