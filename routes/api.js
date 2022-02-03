b486fcca9e9c51a709cef73939ccf26e84667343
Tali <taliitacamargo@icloud.com>
2022-02-03 11:20:28 -0500

    new

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const Workout  = require("../models/Workout");
+const Workout  = require("../models/workout");
 
 
 
@@ -7,7 +7,7 @@ router.get("/api/workouts", (req, res) => {
     Workout.aggregate([
         {
             $addFields: {
-                totalDuration: { $sum: "$exercises.duration" },
+                totalDuration: { $sum: "$exercises.duration " },
             },
         },
     ])
